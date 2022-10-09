4b785fe4462c6f0583424495016b956771a984a5
Narayan Aryal <aryal.coding@gmail.com>
2022-10-09 22:55:47 +1100

    More challanges done

--- index.js
+++ index.js
@@ -75,8 +75,8 @@ function square(number){
     return number * number
 };
 
-let result = square(2);
-console.log(result);
+let res = square(2);
+console.log(res);
 
 
 setTimeout(() => {
@@ -129,3 +129,70 @@ const newFood = food.every((item, i) => {
 })
 console.log(newFood);
 
+const money = [50, 30, 60, 90, 69, 56, 789, 0, 45, 99];
+
+const totalMoney = money.reduce((a, b) => {
+    console.log(a);
+    return a+b;
+}, 0)
+console.log(totalMoney);
+
+// Create an array of 50 random numbers  range between 1 to 100 programmatically
+const result = [];
+for(let i = 0 ; i < 100; i++){
+    const number = Math.floor(Math.random() * 100);
+    console.log(number);
+    result.push(number);
+}
+console.log(result);
+
+const args = [];
+// modify your code below
+
+for (let i = 0; i < 50; i++) {
+  const num = Math.floor(Math.random() * 100) + 1; // 0 - 99
+  args.push(num);
+}
+console.log(args);
+
+// 2. sort that array in descending order
+
+const des = result.sort((a,b) => {
+    return (b-a);
+}) 
+console.log(des);
+
+//3.get the totatl value of the array
+ const  totalValue = result.reduce((a,b) =>{
+    return a +b;
+ })
+
+ console.log(totalValue)
+//4. create new array for odd and even number sets out of the original array
+const odd = result.filter((a, b) => {
+    if(a%2 !== 0){
+        return a;
+    }
+})
+console.log(odd);
+
+const even = result.filter((a, b) => {
+    if( a%2 === 0){
+        return a;
+    }
+})
+console.log(even);
+
+//5. Remove the duplicate values from array
+
+const uniqueValue = [];
+
+for (let i = 0; i < result.length; i++){
+    const pickedValue = result[i];
+    console.log(pickedValue);
+    
+    if(!uniqueValue.includes(pickedValue)){
+        uniqueValue.push(pickedValue);
+    }
+    console.log(uniqueValue);
+}
