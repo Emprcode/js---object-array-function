94a3493528c9d74a10b8b743ad29332778b56f84
Narayan Aryal <aryal.coding@gmail.com>
2022-10-09 20:51:06 +1100

    More added

--- index.js
+++ index.js
@@ -13,6 +13,49 @@ console.log("Hello world");
 
 // Array;
 
+console.log(name);
+var name = "NArayan";
+console.log(name)
+
+// let a =  4;
+// let b = 6;
+// let c = a + b;
+// console.log(c, typeof(c));
+
+// const a = Math.ceil(4.2);
+// const a = Math.floor(4.2);
+// const a = Math.random() * 100;
+// console.log(a);
+
+let age = 16;
+const gender = "male";
+
+if (age >= 18 && gender == "male"){
+    console.log("Welcome to the parrty")
+} else{
+    console.log("Go home");
+}
+
+
+const pet = "dsad";
+switch(pet){
+    case "dog":
+        console.log("Vow vow");
+        break;
+        
+    case "cat":
+        console.log("meow meow");
+        break;
+
+        case "tiger":
+            console.log("yerrr");
+            break;
+
+            default:
+                console.log("NO Animal found");
+}
+
+
 let selectColours = ["red", "blue", "green"];
 selectColours[3] = "black";
 console.log(selectColours);
