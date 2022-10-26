991df7c1feb8e463b86941cdafae5ca0350adcc5
Narayan Aryal <aryal.coding@gmail.com>
2022-10-26 13:19:21 +1100

    some more practice

---
+++ fundamental/app.js
@@ -0,0 +1,14 @@
+// constructer
+
+const Person = (name, sydney,  july) => {
+    this.name = name,
+    this.address = address,
+    this.dob = dob
+    this.draw = () => {
+        console.log("Hey Sup")
+    };
+};
+
+const anotherPerson = new Person;
+
+console.log(anotherPerson());
